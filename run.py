f3f64dda4281afc2e50d664a86054cd1ec405573
Alban Dericbourg <alban@dericbourg.net>
2018-05-02 10:18:25 +0200

    Fix __main__ issue

--- run.py
+++ run.py
@@ -11,15 +11,16 @@ twitter_consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
 twitter_access_token = os.environ['TWITTER_ACCESS_TOKEN']
 twitter_access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
-if len(sys.argv) < 2:
-    print "No image provided"
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "No image provided"
+        sys.exit(1)
 
-api = twitter.Api(consumer_key=twitter_consumer_key,
-                  consumer_secret=twitter_consumer_secret,
-                  access_token_key=twitter_access_token,
-                  access_token_secret=twitter_access_token_secret)
+    api = twitter.Api(consumer_key=twitter_consumer_key,
+                      consumer_secret=twitter_consumer_secret,
+                      access_token_key=twitter_access_token,
+                      access_token_secret=twitter_access_token_secret)
 
-image_path = sys.argv[1]
-file = open(image_path, 'rb')
-api.PostUpdate("", file)
+    image_path = sys.argv[1]
+    file = open(image_path, 'rb')
+    api.PostUpdate("", file)
